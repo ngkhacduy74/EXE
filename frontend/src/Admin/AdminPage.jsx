805cb461d06ef6614a126c3e923f904d318c4054
Nguyễn Hồng Phúc <sonphuc161103@gmail.com>
2025-02-16 12:48:39 +0700

    Error cant connect backend to frontend

---
+++ frontend/src/Admin/AdminPage.jsx
@@ -0,0 +1,12 @@
+// src/Admin/AdminPage.js
+import React from 'react';
+
+const AdminPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Admin Page</h1>
+        </div>
+    );
+};
+
+export default AdminPage;
