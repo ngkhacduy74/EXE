7a620c6877609a408f573c3b586d9eb273df66a2
Nguyễn Hồng Phúc <sonphuc161103@gmail.com>
2025-06-04 13:11:15 +0700

    Revert "Merge branch 'phuc' of https://github.com/ngkhacduy74/EXE into phuc"
    This reverts commit b4366992e41b01b24dfb6deaa231c18a7ddf13f5, reversing
    changes made to 98a68c1fe3e6b8ebec2f2fdf79fa0db768a6e306.

--- backend/Config/openAI.js
+++ backend/Config/openAI.js
@@ -1,25 +1,14 @@
 const axios = require("axios");
 const env = require("dotenv");
-env.config();
-
-async function getChatResponse(userPrompt) {
+async function getChatResponse(prompt) {
   const API_KEY = process.env.OPENAI_API_KEY;
   try {
     const response = await axios.post(
       "https://api.groq.com/openai/v1/chat/completions",
       {
         model: "llama3-70b-8192",
-        messages: [
-          {
-            role: "system",
-            content: process.env.BUSINESS_KNOWLEDGE,
-          },
-          {
-            role: "user",
-            content: userPrompt,
-          },
-        ],
-        max_tokens: 8192,
+        messages: [{ role: "user", content: prompt }],
+        max_tokens: 5000,
       },
       {
         headers: {
@@ -30,7 +19,7 @@ async function getChatResponse(userPrompt) {
     );
     return response.data.choices[0].message.content;
   } catch (err) {
-    console.error("Lỗi Groq:", err.response?.data || err.message);
+    console.error("Lỗi OpenAI:", err.response?.data || err.message);
     throw new Error("Gọi Groq thất bại.");
   }
 }
--- backend/Router/auth.route.js
+++ backend/Router/auth.route.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { authMiddleware } = require("../Middleware/index");
-const {
-  Login,
-  Register,
-  Refresh_Token,
-} = require("../Controller/auth.controller");
+const { Login, Register } = require("../Controller/auth.controller");
 
 router.post("/login", async (req, res) => {
   const result = await Login(req.body);
@@ -23,18 +19,4 @@ router.post("/register", authMiddleware.validateUser, async (req, res) => {
   res.status(200).json(result);
 });
 
-router.post("/refresh-token", async (req, res) => {
-  const refresh = req.body;
-
-  if (!refresh) {
-    return res
-      .status(400)
-      .json({ success: false, error: "Thiếu refresh token" });
-  }
-  const result = await Refresh_Token(refresh);
-  if (result.success === false) {
-    return res.status(500).json(result);
-  }
-  res.status(200).json(result);
-});
 module.exports = router;
