f5253ea548e4a90073a261bb24ec67211bff0edc
ngkhacduy74 <duycaduoc0704@gmail.com>
2025-06-05 17:07:32 +0700

    phan quyen

--- backend/Middleware/auth.middleware.js
+++ backend/Middleware/auth.middleware.js
@@ -8,6 +8,38 @@ const validateUser = (req, res, next) => {
   }
   next();
 };
+
+const verifyToken = (req, res, next) => {
+  try {
+    const token = req.headers.token;
+
+    if (!token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorized: Token không được cung cấp" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    if (
+      !decoded ||
+      decoded.user.role !== "Admin" ||
+      decoded.user.role !== "User"
+    ) {
+      return res
+        .status(403)
+        .json({ message: "Forbidden: Bạn không có quyền truy cập" });
+    }
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(403).json({
+      message: "Token không hợp lệ hoặc đã hết hạn",
+      error: error.message,
+    });
+  }
+};
 const verifyAdmin = (req, res, next) => {
   try {
     const token = req.headers.token;
@@ -52,7 +84,7 @@ const verifyUser = (req, res, next) => {
         .json({ message: "Forbidden: Bạn không có quyền truy cập" });
     }
 
-    req.user = decoded; 
+    req.user = decoded;
     next();
   } catch (error) {
     return res.status(403).json({
@@ -65,4 +97,5 @@ module.exports = {
   validateUser,
   verifyAdmin,
   verifyUser,
+  verifyToken,
 };
